e4720d0976abe92f84714c100d6a4f38aa416c1f
Adela Sobotkova <adela@fedarch.org>
2021-05-04 23:07:27 +0200

    application of f.prom to Yambol and mounds

---
+++ scripts/Focal_Yam.R
@@ -0,0 +1,73 @@
+#########Focal Yambol
+
+# libraries
+
+library(raster)
+library(mapview)
+library(tidyverse)
+library(sf)
+
+# get raster data
+yam <- getData('SRTM', lon = 9, lat= 56)
+plot(bg1)
+bg2 <- getData('SRTM', lon = 11, lat= 56)
+plot(bg2)
+bg3 <- getData('SRTM', lon = 9, lat= 51)
+plot(bg3)
+bg4 <- getData('SRTM', lon = 11, lat= 51)
+plot(bg4)
+
+
+bg <- mosaic(bg1, bg2, bg3, bg4, fun = mean)
+
+
+mapview(bg)
+
+
+plot(bg)
+e <- drawExtent(show = TRUE, col = "red")
+bg_crop <- crop(bg, e)
+bg_crop <- projectRaster(bg_crop, crs = 32632)
+
+
+mapview(bg_crop) + mapview(bg_borders)
+
+
+# get elevation data
+bg_el <- getData("alt", country = "BG", mask = TRUE)
+plot(bg_el)
+hist(bg_el)
+
+# get admin data
+bg_borders <- getData("GADM", country = "BG", level = 0)
+bg_regions <- getData("GADM", country = "BG", level = 1)
+plot(bg_el); plot(bg_regions, add=TRUE)
+
+
+bg_regions <- st_as_sf(bg_regions)
+Yambol <- bg_regions %>% 
+  dplyr::filter(NAME_1=="Yambol")
+
+Yam_el <- crop(bg_el, Yambol)
+plot(Yam_el)
+
+
+# Calculate prominence
+library(FSA)
+# run f.prom function first
+f.prom <- function(x) perc(x,x[length(x)/2],"lt")
+
+# Apply it in focal environment on pixels rather than meters of radius (as it's un-projected)
+yam_prompix21 <- focal(Yam_el, w=matrix(1,nrow=21,ncol=21), fun=f.prom) #10 pixel radius 
+plot(yam_prompix21)
+hist(yam_prompix21)
+
+bg_prompix21 <- focal(bg_el, w=matrix(1,nrow=21,ncol=21), fun=f.prom)
+plot(bg_prompix21)
+
+
+# Load mounds
+
+mounds <- st_read("landscape_prominence/data/mounds.shp")
+mounds$prom21pix <- raster::extract(bg_prompix21, st_transform(mounds, 4326))
+hist(yam_prompix21); hist(mounds$prom21pix, add =TRUE)
